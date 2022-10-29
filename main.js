c6c4eed65eeda0c3b9ee4f9135cd334c5f9dcfd7
muhammedazlan11@gmail.com <muhammedazlan11@gmail.com>
2022-10-29 06:56:49 +0530

    main

--- main.js
+++ main.js
@@ -15,7 +15,9 @@
     window.onscroll = ()=>{
       this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
     }
-    /* slides */
+ 
+/* function for ep1 */
+   /* slides */
     let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -41,31 +43,46 @@ function showSlides(n) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  dots[slideIndex-1] += "active";
 }
-/* function for ep1 */
-function hide(){
+/* hide unhide*/
+function hide1(){
   
  document.getElementById("background").className='thumbnailblur';
  
-  document.getElementById('hide').style.display="block";
+  document.getElementById('hide1').style.display="block";
   
-  document.getElementById('blur').style.display="block";
+  document.getElementById('blur1').style.display="block";
 }
-function hideagain(){
+function hideagain1(){
   
   
  document.getElementById("background").className='thumbnail';
  
-  document.getElementById('hide').style.display="none"; 
+  document.getElementById('hide1').style.display="none"; 
   
-  document.getElementById('blur').style.display="none";
+  document.getElementById('blur1').style.display="none";
 }
- 
-  /* typing animation*/
-    var typed = new Typed(".typing", {
-      strings: ["Developer", "Designer", "Freelancer"],
-      typeSpeed: 100,
-      backSpeed: 60,
-      loop: true
-    });
+
+/* function for ep2*/
+   
+/* hide unhide*/
+function hide2() {
+
+  document.getElementById("background").className = 'thumbnailblur';
+
+  document.getElementById('hide2').style.display = "block";
+
+  document.getElementById('blur2').style.display = "block";
+}
+
+function hideagain2() {
+
+
+  document.getElementById("background").className = 'thumbnail';
+
+  document.getElementById('hide2').style.display = "none";
+
+  document.getElementById('blur2').style.display = "none";
+}
+
